a8e28b002cbdd37fce3982891f7e862ca21c9334
Justin Braun <JustinBraun1999@web.de>
2019-07-22 07:52:11 +0200

    Init

---
+++ SocketClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SocketClient {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
